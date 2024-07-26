1540720d74227acd14c5517021aa8b2abf17af71
Iain Dillingham <iain@dillingham.me.uk>
2024-07-26 12:19:59 +0100

    Add `chronic_cardiac_disease`
    To do so, we replace the CTV3 codelist with the equivalent SNOMED CT
    codelist.

--- analysis/dataset_definition.py
+++ analysis/dataset_definition.py
@@ -1,5 +1,10 @@
-from ehrql import create_dataset
+from ehrql import codelist_from_csv, create_dataset
+from ehrql.tables.core import clinical_events
 from ehrql.tables.tpp import patients, practice_registrations
+
+chronic_cardiac_disease_codes = codelist_from_csv(
+    "codelists/opensafely-chronic-cardiac-disease-snomed.csv", column="id"
+)
 
 dataset = create_dataset()
 
@@ -12,3 +17,12 @@ has_registration = practice_registrations.for_patient_on(
 dataset.define_population(has_registration)
 
 dataset.sex = patients.sex
+
+dataset.chronic_cardiac_disease = (
+    clinical_events.where(
+        clinical_events.snomedct_code.is_in(chronic_cardiac_disease_codes)
+    )
+    .sort_by(clinical_events.date)
+    .first_for_patient()
+    .date
+)
