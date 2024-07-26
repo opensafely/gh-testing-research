bc82c836d1951032ff43fc0214ff93d47bd2b51a
Iain Dillingham <iain@dillingham.me.uk>
2024-07-26 13:17:45 +0100

    Add `bp_sys`
    To do so, we replace the CTV3 code with the equivalent SNOMED CT code.

--- analysis/dataset_definition.py
+++ analysis/dataset_definition.py
@@ -11,6 +11,8 @@ chronic_liver_disease_codes = codelist_from_csv(
 salbutamol_codes = codelist_from_csv(
     "codelists/opensafely-asthma-inhaler-salbutamol-medication.csv", column="id"
 )
+# Systolic arterial pressure. Corresponds to CTV3 "2469."
+systolic_blood_pressure_codes = ["72313002"]
 
 dataset = create_dataset()
 
@@ -41,6 +43,20 @@ dataset.chronic_liver_disease = (
     .first_for_patient()
     .date
 )
+
+# https://github.com/opensafely/risk-factors-research/issues/48
+bp_sys_clinical_events = clinical_events.where(
+    clinical_events.snomedct_code.is_in(systolic_blood_pressure_codes)
+)
+date_of_most_recent_bp_sys_clinical_event = (
+    bp_sys_clinical_events.where(clinical_events.date.is_on_or_before("2020-02-01"))
+    .sort_by(clinical_events.date)
+    .last_for_patient()
+    .date
+)
+dataset.bp_sys = bp_sys_clinical_events.where(
+    clinical_events.date == date_of_most_recent_bp_sys_clinical_event
+).numeric_value.mean_for_patient()
 
 dataset.recent_salbutamol_count = (
     medications.where(medications.dmd_code.is_in(salbutamol_codes))
