c219d31ab1ba7bb7946ecb34ffed0ba5ebd5bf42
Iain Dillingham <iain@dillingham.me.uk>
2024-07-26 12:28:49 +0100

    Add `chronic_liver_disease`
    To do so, we replace the CTV3 codelist with the equivalent SNOMED CT
    codelist.

--- analysis/dataset_definition.py
+++ analysis/dataset_definition.py
@@ -5,6 +5,9 @@ from ehrql.tables.tpp import patients, practice_registrations
 chronic_cardiac_disease_codes = codelist_from_csv(
     "codelists/opensafely-chronic-cardiac-disease-snomed.csv", column="id"
 )
+chronic_liver_disease_codes = codelist_from_csv(
+    "codelists/opensafely-chronic-liver-disease-snomed.csv", column="id"
+)
 
 dataset = create_dataset()
 
@@ -21,6 +24,15 @@ dataset.sex = patients.sex
 dataset.chronic_cardiac_disease = (
     clinical_events.where(
         clinical_events.snomedct_code.is_in(chronic_cardiac_disease_codes)
+    )
+    .sort_by(clinical_events.date)
+    .first_for_patient()
+    .date
+)
+
+dataset.chronic_liver_disease = (
+    clinical_events.where(
+        clinical_events.snomedct_code.is_in(chronic_liver_disease_codes)
     )
     .sort_by(clinical_events.date)
     .first_for_patient()
